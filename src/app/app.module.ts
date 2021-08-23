8e439d50d12acae9e7e1a2cb6d3c13e2f558eb7f
Guilherme Fujii <gui.fujii@outlook.com>
2021-08-23 19:24:18 -0300

    fix: added modules to root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountModule } from './modules/account/account.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccountModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
