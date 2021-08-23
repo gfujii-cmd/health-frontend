b55ee8072fd4d007d232423d536923ea23349c22
Guilherme Fujii <gui.fujii@outlook.com>
2021-08-23 19:38:01 -0300

    style: added the wide button to login component

--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { AccountRoutingModule } from './account-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -8,7 +9,8 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
   imports: [
     CommonModule,
-    AccountRoutingModule
+    AccountRoutingModule,
+    SharedModule
   ],
   exports: [],
   declarations: [
