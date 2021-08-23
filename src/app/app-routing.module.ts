fa58df2f47cd4e0b091e834048dded5677aaadb0
Guilherme Fujii <gui.fujii@outlook.com>
2021-08-23 19:22:21 -0300

    feat: default routes with lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'account',
+    loadChildren: () => import('./modules/account/account.module').then(m => m.AccountModule)
+  },
+  {
+    path: '',
+    redirectTo: '/account/login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
