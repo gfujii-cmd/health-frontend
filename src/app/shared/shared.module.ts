d54fc243df54b848f0b1b3d82cdc0b7fef5ab8d1
Guilherme Fujii <gui.fujii@outlook.com>
2021-08-23 19:23:17 -0300

    feat: shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent
+  ],
+  declarations: [
+    HeaderComponent
+  ],
+  providers: [],
+})
+export class SharedModule { }
