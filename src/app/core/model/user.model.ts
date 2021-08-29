ec90135d0c6278d830f4a6abc56803d69612968e
Guilherme Fujii <gui.fujii@outlook.com>
2021-08-29 11:30:31 -0300

    new: User Service

---
+++ src/app/core/model/user.model.ts
@@ -0,0 +1,7 @@
+export interface User {
+  name: string,
+  lastName: string,
+  email: string,
+  score: number,
+  job: string
+}
---
+++ src/app/core/services/user/user.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { BaseResponse } from '../../model/response/base-response.model';
+import { User } from '../../model/user.model';
+
+const API_URL = environment.apiUrl + '/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getUser(email: string): Observable<BaseResponse<User>> {
+    return this.http.get<BaseResponse<User>>(API_URL);
+  }
+}
