c77bfbe0db132e5208d2fa4282d17c60d95a384d
Guilherme Fujii <gui.fujii@outlook.com>
2021-09-23 21:43:07 -0300

    new: user request for showing the user hand wash graph

--- src/app/modules/user/user-panel/user-panel.component.ts
+++ src/app/modules/user/user-panel/user-panel.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
+import { HandWashResponse } from 'src/app/core/model/response/hand-wash-response.model';
 import { TokenStorageService } from 'src/app/core/services/auth/token-storage.service';
+import { HandWashService } from 'src/app/core/services/hand-wash/hand-wash.service';
 
 @Component({
   selector: 'app-user-panel',
@@ -13,13 +15,22 @@ export class UserPanelComponent implements OnInit {
 
   userName!: string;
 
+  userEmail!: string;
+
+  handWashData!: HandWashResponse;
+
+  data: number[] = [];
+
   constructor(
     private router: Router,
-    private token: TokenStorageService
+    private token: TokenStorageService,
+    private handWashService: HandWashService
     ) { }
 
   ngOnInit(): void {
     this.userName = this.token.getUser();
+    this.userEmail = this.token.getEmail();
+    this.getWashData();
   }
 
   public barChartOptions: ChartOptions = {
@@ -32,7 +43,7 @@ export class UserPanelComponent implements OnInit {
   public barChartLegend = true;
 
   public barChartData: ChartDataSets[] = [
-    { data: [18, 10, 23, 5, 10, 6, 12], label: 'Lavagens/Dia' }
+    { data: this.data, label: 'Lavagens/Dia' }
   ];
 
   public height = 400
@@ -41,4 +52,21 @@ export class UserPanelComponent implements OnInit {
     this.router.navigate([`${page}`])
   }
 
+  getWashData(){
+    this.handWashService.getWashData(this.userEmail).subscribe(data => {
+      if (data.response && 200 === data.httpCode) {
+        this.handWashData = data.response;
+        this.pushValuesToDataArray();
+      }
+    }, err => {
+      console.log(err.error);
+    })
+  }
+
+  pushValuesToDataArray(){
+    const data = Object.values(this.handWashData);
+    for(var value in data){
+      this.data.push(data[value]);
+    }
+  }
 }
